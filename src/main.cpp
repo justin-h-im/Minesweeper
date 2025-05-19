b79ee1b256d5e78adea93d66f3ef2bc15cfdb17b
justin-h-im <imjustin679@gmail.com>
2025-05-19 15:04:36 -0700

    main setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,70 @@
-#include <Arduino.h>
+// all of these headers are provided from the CS120B course
+#include "helper.h"
+#include "spiAVR.h"
+#include "periph.h"
+#include "serialATmega.h"
+#include "timerISR.h"
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Task struct for concurrent synchSMs implmentations (provided)
+typedef struct _task{
+  //Task's current state
+	signed char state;
+  // Task period
+	unsigned long period;
+  //Time elapsed since last task tick
+	unsigned long elapsedTime;
+  //Task tick function
+	int (*TickFct)(int); 		
+} task;
+
+
+// task array size define
+#define NUM_TASKS 3
+
+// pin defines
+
+// period definitions
+const unsigned long GCD_PERIOD = 50;
+
+// task array
+task tasks[NUM_TASKS];
+
+// task enums
+
+
+// task definitions
+
+
+// executes tasks
+void TimerISR() {   
+  // Iterate through each task in the task array
+	for (unsigned int i = 0; i < NUM_TASKS; i++ ) {
+    // Check if the task is ready to tick    
+		if ( tasks[i].elapsedTime == tasks[i].period ) {
+      // Tick and set the next state for this task      
+			tasks[i].state = tasks[i].TickFct(tasks[i].state); 
+      // Reset the elapsed time for the next tick
+			tasks[i].elapsedTime = 0;                          
+		}
+    // Increment the elapsed time by GCD_PERIOD
+		tasks[i].elapsedTime += GCD_PERIOD;
+	}
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int main() {
+  // gpio initialization
+
+  // concurrent fsm task initialization
+
+
+  // timer initialization
+  TimerSet(GCD_PERIOD);
+  TimerOn();
+
+  // main loop
+  while (1) {}
+
+  return 0;
 }
