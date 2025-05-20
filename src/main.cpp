03f9a23e7feaeae403f524b3f3d7eae5280cdcc8
justin-h-im <imjustin679@gmail.com>
2025-05-20 02:38:03 -0700

    operational color changing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "periph.h"
 #include "serialATmega.h"
 #include "timerISR.h"
+#define F_CPU 16000000UL // 16 MHz
+#include <util/delay.h>
 
 // helper functions declarations
 // GPIO initialization, call before tasks in main
@@ -15,6 +17,7 @@ void spiWriteCommand(uint8_t command);
 
 void spiWriteData(uint8_t data);
 
+void fillRect(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint16_t color);
 
 // Task struct for concurrent synchSMs implmentations (provided)
 typedef struct _task{
@@ -32,26 +35,26 @@ typedef struct _task{
 // task array size define
 #define NUM_TASKS 1
 
-// pin defines
-/*
-  PC0 -> LCD LED Pin
-  PC1 -> LCD A0 Pin
-  PC2 -> LCD Reset Pin
-  PC3 -> LCD CS Pin
-  PC4 -> I2C SDA
-  PC5 -> I2C SCL
-  
-  PB0 -> Joystick Vrx
-  PB1 -> Joystick Vry
-  PB3 -> LCD SDA Pin		
-  PB5-> LCD SCK Pin
+// // pin defines
+// /*
+//   PB0 -> Joystick Vrx
+//   PB1 -> Joystick Vry
+//   PB3 -> LCD SDA Pin		
+//   PB5-> LCD SCK Pin
 
-  PD2 -> Start Button
-  PD3 -> Reset Button
-  PD4 -> “1 or 2” Player Button
-  PD5 -> Select Button
-  PD6 -> Buzzer Output
-*/
+//   PC0 -> LCD LED Pin
+//   PC1 -> LCD A0 Pin
+//   PC2 -> LCD Reset Pin
+//   PC3 -> LCD CS Pin
+//   PC4 -> I2C SDA
+//   PC5 -> I2C SCL
+
+//   PD2 -> Start Button
+//   PD3 -> Reset Button
+//   PD4 -> “1 or 2” Player Button
+//   PD5 -> Select Button
+//   PD6 -> Buzzer Output
+// */
 
 // Port B
 #define JOYSTICK_VRX PB0
@@ -63,7 +66,7 @@ typedef struct _task{
 #define LCD_LED PC0 // (output)
 #define LCD_A0 PC1 // (output)
 #define LCD_RESET PC2 // (output)
-#define LCD_CS PC3 // LCD Chip Select (output)
+#define LCD_CS PB3 // LCD Chip Select (SS) (output)
 #define I2C_SDA PC4
 #define I2C_SCL PC5
 
@@ -74,17 +77,23 @@ typedef struct _task{
 #define SELECT_BUTTON PD5
 #define BUZZER PD6
 
+// LCD command defines
+#define SWRESET 0x01
+#define CASET 0x2A
+#define RASET 0x2B
+#define RAMWR 0x2C
+
 
 // period definitions
 const unsigned long GCD_PERIOD = 50;
 
-// task array
+// // task array
 task tasks[NUM_TASKS];
 
-// task enums
+// // task enums
 
 
-// task definitions
+// // task definitions
 
 
 // executes tasks
@@ -103,30 +112,34 @@ void TimerISR() {
 	}
 }
 
-
 int main() {
   // gpio initialization
   gpioInit();
 
-  // LCD initialization (goal is white screen)
+  // LCD initialization
   lcdInit();
+  fillRect(0, 0, 127, 127, 0xF800);
+  // test, fill the screen with red
 
   // concurrent fsm task initialization
 
   // timer initialization
-  TimerSet(GCD_PERIOD);
-  TimerOn();
+  // TimerSet(GCD_PERIOD);
+  // TimerOn();
 
-  // main loop
-  while (1) {}
+
+  // main loop (should never reach here)
+  while (1) { fillRect(0, 0, 127, 127, 0x001F); }  // fill screen with red
 
   return 0;
 }
 
+// GPIO initialization
 void gpioInit() {
+
   // SPI pins
   // configure SCK and MOSI as output
-  DDRB |= (1 << LCD_SCK) | (1 << LCD_SDA);
+  DDRB |= (1 << LCD_SCK) | (1 << LCD_SDA) | (1 << PB2);;
   // configure LED, A0, Reset, and CS as output
   DDRC |= (1 << LCD_LED) | (1 << LCD_A0) | (1 << LCD_RESET) | (1 << LCD_CS);
 
@@ -136,10 +149,17 @@ void gpioInit() {
   // Set initial states for outputs
   // etc.
 
-  // enable spi, set as master, set clock rate fck/16
-  SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0); 
+  PORTC |= (1 << PB2); // pull SS
+  PORTC |= (1 << LCD_CS); // pull cs high
+  PORTC |= (1 << LCD_A0); // pull A0 high
+  PORTC |= (1 << LCD_RESET); // pull reset high
+
+  // enable spi, set as master
+  SPCR = (1<<SPE) | (1<<MSTR);
+  // SPSR = (1 << SPI2X);
 }
 
+// send command to the LCD
 void spiWriteCommand(uint8_t command) {
   // pull A0 low to specify command
   PORTC &= ~(1 << LCD_A0);
@@ -154,6 +174,7 @@ void spiWriteCommand(uint8_t command) {
   PORTC |= (1 << LCD_CS);
 }
 
+// send data to the LCD
 void spiWriteData(uint8_t data) {
   // pull A0 high to specify data
   PORTC |= (1 << LCD_A0);
@@ -168,14 +189,35 @@ void spiWriteData(uint8_t data) {
   PORTC |= (1 << LCD_CS);
 }
 
+// fills the enture screen wth a color
+void fillRect(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint16_t color) {
+  uint32_t count = (uint32_t)(x1 - x0 + 1) * (y1 - y0 + 1);
 
+  // Set window
+  spiWriteCommand(CASET);
+  spiWriteData(0x00); spiWriteData(x0);
+  spiWriteData(0x00); spiWriteData(x1);
+
+  spiWriteCommand(RASET);
+  spiWriteData(0x00); spiWriteData(y0);
+  spiWriteData(0x00); spiWriteData(y1);
+
+  // Write pixels
+  spiWriteCommand(RAMWR);
+  for (uint32_t i = 0; i < count; ++i) {
+    spiWriteData(color >> 8);
+    spiWriteData(color & 0xFF);
+  }
+}
+
+// LCD initialization
 void lcdInit() {
   // lcd initialization, refer to st7735 datasheet for details
 
   // hardware reset
   PORTC &= ~(1 << LCD_RESET); // bring reset low
   _delay_ms(200);
-  PORTC &= (1 << LCD_RESET); // bring reset high
+  PORTC |= (1 << LCD_RESET); // bring reset high
   _delay_ms(200);
 
   // initialize SPI
@@ -183,12 +225,10 @@ void lcdInit() {
   _delay_ms(150);
   spiWriteCommand(0x11); // send SLPOUT command
   _delay_ms(200);
+
   spiWriteCommand(0x3A); // send COLMOD command
-  spiWriteCommand(0x05); // set mode to 16-bit color
+  spiWriteData(0x05); // set mode to 16-bit color
   _delay_ms(10);
   spiWriteCommand(0x29); // send DISPON command
   _delay_ms(200);
-
-  // enable backlight
-  PORTC |= (1 << LCD_LED);
 }
